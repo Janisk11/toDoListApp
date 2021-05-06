bf33874ddb1a9990a056d6e2877887291446fb7d
janis <janiskjoseph@gmail.com>
2021-05-07 01:18:58 +0530

    :first commit

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-// js code for ToDo List page
-// prevent back
 
-function preventBack() { window.history.forward(); }  
-setTimeout("preventBack()", 0);  
-window.onunload = function () { null }; 
+// prevent back 
+    function preventBack() { window.history.forward(); }  
+    setTimeout("preventBack()", 0);  
+    window.onunload = function () { null }; 
+    
+// focusing username when page loads
+    function firstfocus()
+    {
+    document.logform.username.focus();
+    return true;
+    }
+    
+//  validate login form and redirect to main page  
+    function validatelogin(callback){
+        var user="admin";
+        var pwd = 12345;
+        let username = document.getElementById("username").value;
+        let logpwd = document.getElementById("logpwd").value;
+    
+        if(username == "" || logpwd == ""){
+            alert("Username and Password cannot be empty");
+            return false
+        }
+        else if(username != user || logpwd != pwd){
+            alert("Incorrect Username or Password");
+            return false
+    
+        }
+        else {
+            callback();
+        }
+    }
+    
+    function loadmain(){ 
+         return true;
+    }
 
 
     // fetch data from api and arrange in a table with checkboxes
@@ -75,3 +106,22 @@ window.onunload = function () { null };
                     });
     
     });
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
